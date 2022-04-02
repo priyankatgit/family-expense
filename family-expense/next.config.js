9d43dbd89f8251d6787ad79c48e69d83748b049f
Priyank <mailatpriyank@gmail.com>
2022-04-02 11:26:55 +0530

    Fix: Live domain mapping

--- family-expense/next.config.js
+++ family-expense/next.config.js
@@ -4,7 +4,7 @@ const dev = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  server: dev ? "http://localhost:3000" : "https://anything.vercel.app",
+  server: dev ? "http://localhost:3000" : "https://fme.vercel.app",
 }
 
 module.exports = nextConfig
